392eea2f459c417319e289276f858c3c07b7c758
Tim Wheeler <tim@timwheeler.com>
2019-05-28 22:12:47 -0400

    added countdown timer to status bar

---
+++ app/MainWindow.js
@@ -0,0 +1,27 @@
+const electron = require('electron');
+
+const { BrowserWindow } = electron;
+
+class MainWindow extends BrowserWindow {
+    constructor(url) {
+        super({
+            height: 500,
+            width: 300,
+            frame: false,
+            resizable: false,
+            show: false,
+            webPreferences: {
+                nodeIntegration: true,
+                backgroundThrottling: false
+            }
+        });
+        this.loadURL(url);
+        this.on('blur', this.onBlur.bind(this))
+    }
+
+    onBlur() {
+        this.hide();
+    }
+}
+
+module.exports = MainWindow;
--- index.js
+++ index.js
@@ -1,50 +1,25 @@
 const path = require('path');
 const electron = require('electron')
+const TimerTray = require('./app/TimerTray');
+const MainWindow = require('./app/MainWindow');
 
-const { app, BrowserWindow, Tray } = electron;
+
+const { app, ipcMain } = electron;
 
 let mainWindow;
 let tray;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        height: 500,
-        width: 300,
-        frame: false,
-        resizable: false,
-        show: false,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+    app.dock.hide();
+    mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
 
     const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
-    tray = new Tray(iconPath);
-
-    tray.on('click', (event, bounds) => {
-
-        // click event bounds
-        const { x, y } = bounds;
-
-        // window height and width
-        const { height, width } = mainWindow.getBounds();
-
-        console.log(bounds.x, bounds.y)
-        if (mainWindow.isVisible()) {
-            mainWindow.hide();
-        } else {
-            const yPosition = process.platform === 'darwin' ? y : y - height;
-            mainWindow.setBounds({
-                x: x - (width / 2),
-                y: yPosition,
-                height,
-                width
-            });
-            mainWindow.show();
-        }
-    })
+    tray = new TimerTray(iconPath, mainWindow);
 
 });
+
+ipcMain.on('update-timer', (event, timeLeft) => {
+    tray.setTitle(timeLeft);
+})
